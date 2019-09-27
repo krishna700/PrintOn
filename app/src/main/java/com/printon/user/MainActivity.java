8360c82c19312d7c1645ca6fb7841032c18f354e
Divij Sethi <dvjsethi98@gmail.com>
2019-09-28 01:28:36 +0530

    Test

--- app/src/main/java/com/printon/user/MainActivity.java
+++ app/src/main/java/com/printon/user/MainActivity.java
@@ -3,12 +3,22 @@ package com.printon.user;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+//  Git test
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void check(View view) {
+        Toast.makeText(this, "This is test", Toast.LENGTH_SHORT).show();
     }
 }
